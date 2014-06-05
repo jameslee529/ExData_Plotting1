44a9fb52026deade2f9f265b3ee4e0196e755224
James Lee <jameslee529sh@gmail.com>
2014-06-05 22:27:35 +0800

    kick off

---
+++ plot1.R
@@ -0,0 +1,13 @@
+#Loading data
+data <- read.table("household_power_consumption.txt", header = T, sep = ";", na.strings = "?", 
+                   col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+                   nrows = 60 * 24 * 5, skip = 66600)
+
+#convertint the Date and Time variables to Date/Time classes
+data$Time <- paste(data$Date, data$Time)
+data$Time <- strptime(data$Time, format = "%d/%m/%Y %H:%M:%S")
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+
+#subsetting 
+data <- data[data$Date %in% as.Date(c("2007-02-01", "2007-02-02")), ]
+
