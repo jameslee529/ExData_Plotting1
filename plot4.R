c49dd5426459b8f603a4b3d20732533430ec646c
James Lee <jameslee529sh@gmail.com>
2014-06-06 11:06:27 +0800

    add plot 4

---
+++ plot4.R
@@ -0,0 +1,31 @@
+#Loading data
+data <- read.table("household_power_consumption.txt", header = T, sep = ";", na.strings = "?", 
+                   col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+                   nrows = 60 * 24 * 4, skip = 66600)
+
+#convertint the Date and Time variables to Date/Time classes
+data$Time <- paste(data$Date, data$Time)
+data$Time <- strptime(data$Time, format = "%d/%m/%Y %H:%M:%S")
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+
+#subsetting 
+data <- data[data$Date %in% as.Date(c("2007-02-01", "2007-02-02")), ]
+
+#plot and send to PNG image file
+Sys.setlocale(category = "LC_ALL", locale = "English")
+par(mfrow = c(2, 2), mar = c(4, 4, 2, 1), oma = c(0, 0, 2, 0))
+with(data, {
+  plot(Time, Global_active_power, type = "l" , xlab = "", ylab = "Global Active Power")
+  
+  plot(Time, Voltage, type = "l" , xlab = "datetime", ylab = "Voltage")
+  
+  plot(Time, Sub_metering_1, type = "l" , xlab = "", ylab = "Energy sub metering")
+  points(Time, Sub_metering_2, type = "l" , col = "red")
+  points(Time, Sub_metering_3, type = "l" , col = "blue")
+  legend("topright", pch = "_", col = c("black", "blue", "red"), 
+         legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  
+  plot(Time, Global_reactive_power, type = "l" , xlab = "", ylab = "Global Reactive Power")
+})
+dev.copy(png, file = "plot4.png", width = 480, height = 480, units = "px", pointsize = 10)
+dev.off() 
